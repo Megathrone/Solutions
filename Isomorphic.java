934f4366d1c79e5114b236ad6b5dbcc75e8dc809
Megathrone <yizhouwang418@gmail.com>
2018-02-14 10:55:56 +0800

    1

---
+++ Isomorphic.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+
+
+/**
+ * Isomorphic
+ */
+public class Isomorphic {
+    public static boolean isIsomorphic(String s1, String s2) {
+       
+        Map<Character, Integer> m1 = new HashMap<>();
+        Map<Character, Integer> m2 = new HashMap<>();
+    
+        for(Integer i = 0; i < s1.length(); i++) {
+            
+            System.out.println(s1.charAt(i));
+            if(m1.put(s1.charAt(i), i) != m2.put(s2.charAt(i), i)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+
+    public static void main(String[] args) {
+        System.out.println(isIsomorphic("abb", "egg"));
+        Map<Character, Integer> m1 = new HashMap<>();
+        Map<Character, Integer> m2 = new HashMap<>();
+        m1.put('a',1);
+        System.out.println(m1.put('a', 2));
+
+    }
+}
