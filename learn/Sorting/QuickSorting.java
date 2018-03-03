08a55be79056c34c662efc1557633af344cdfdc9
Megathrone <yizhouwang418@gmail.com>
2018-03-03 15:28:41 +0800

    快速排序

---
+++ learn/Sorting/QuickSorting.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+
+/**
+ * QuickSorting
+ */
+public class QuickSorting {
+
+    public static void sort(int[] a) {
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(int[] a, int lo, int hi) {
+        if (hi <= lo)
+            return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+
+    private static int partition(int[] a, int lo, int hi) {
+        int i = 0;
+        int j = hi + 1;
+
+        while (true) {
+            while (less(a[++i], a[lo])) {
+                if (i == hi)
+                    break;
+            }
+            while (less(a[lo], a[--j])) {
+                if (j == lo)
+                    break;
+            }
+
+            if (i >= j)
+                break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+
+    public static boolean less(int v, int w) {
+        return v < w;
+    }
+
+    public static void exch(int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    public static boolean isSorted(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1]))
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] a = { 4, 5, 6, 1, 2, 4, 5, 7, 1, 13, 4 };
+        sort(a);
+        System.out.println(isSorted(a));
+        System.out.println(Arrays.toString(a));
+    }
+}
