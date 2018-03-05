cef91a443fa7279f53499e5aa1d06b4adfb402fd
Megathrone <yizhouwang418@gmail.com>
2018-03-05 11:47:22 +0800

    树的部分先告一段落

---
+++ src/BalancedBinaryTree.java
@@ -0,0 +1,28 @@
+/**
+ * BalancedBinaryTree
+ */
+public class BalancedBinaryTree {
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null)
+            return true;
+
+        int leftDepth = dfs(root.left);
+        int rightDepth = def(root.right);
+
+        if (Math.abs(leftDepth - rightDepth) > 1) {
+            return false;
+        }
+
+        return isBalanced(root.left) && isBalanced(root.right);
+
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null)
+            return 0;
+        else
+            return Math.max(dfs(node.left), dfs(node.right))+1;
+
+    }
+}
