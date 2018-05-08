facba12e35209972e0886c87fc46eafc60efaf05
Megathrone <yizhouwang418@gmail.com>
2018-05-08 17:33:50 +0800

    合并二叉树，可以以t1为根节点合并，也可以新声明一个。然后注意空条件交换返回就填补空白了

---
+++ src/MergeTwoBinaryTrees.java
@@ -0,0 +1,16 @@
+/**
+ * MergeTwoBinaryTrees
+ */
+public class MergeTwoBinaryTrees {
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(null == t1) return t2;
+        if(null == t2) return t1;
+
+        TreeNode ans = new TreeNode(t1.val+t2.val);
+        ans.left = mergeTrees(t1.left, t2.left);
+        ans.right = mergeTrees(t1.right, t2.right);
+
+        return ans;
+    }
+}
