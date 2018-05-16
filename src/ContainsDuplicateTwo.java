d1097b22095acde1c1106bcdb0f7c454a28df368
Megathrone <yizhouwang418@gmail.com>
2018-05-16 16:51:34 +0800

    HashSet and maintain window

---
+++ src/ContainsDuplicateTwo.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * ContainsDuplicateTwo
+ * 
+ * 解题的关键是要maintain一个window，根据题目给的k值维护一个相同大小的窗口，
+ * 相当于固定游标卡尺在array移动
+ */
+public class ContainsDuplicateTwo {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        // corner case
+        if (nums == null || nums.length <= 1) {
+            return false;
+        }
+        // Set + window
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+            // maintain window
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        // return
+        return false;
+    }
+}
