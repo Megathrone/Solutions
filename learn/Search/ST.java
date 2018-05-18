6c900a4a446abb568a35566e0d1c02ceb761fe21
Megathrone <yizhouwang418@gmail.com>
2018-05-18 11:50:38 +0800

    ST

---
+++ learn/Search/ST.java
@@ -0,0 +1,39 @@
+/**
+ * ST
+ * 
+ * 符号表基础数据结构实现的API
+ */
+public class ST<Key,Value> {
+    ST(){
+
+    }
+
+    void put(Key key, Value value){
+
+    }
+
+    Value get(Key key){
+        
+    }
+
+    void delete(Key key){
+
+    }
+
+    boolean contains(Key key){
+
+    }
+
+    boolean isEmpty(){
+
+    }
+
+    int size(){
+
+    }
+
+    Iterable<Key> keys(){
+        
+    }
+    
+}
