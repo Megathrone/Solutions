901cbd53c5b1c46364e8badf75aa41b3ce86d649
Megathrone <yizhouwang418@gmail.com>
2018-05-20 19:32:01 +0800

    Multipro

---
+++ learn/multiprocess/Demo.java
@@ -0,0 +1,37 @@
+/**
+ * Demo
+ */
+public class Demo {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread previousThread = Thread.currentThread();
+        for (int i = 1; i <= 10; i++) {
+            Thread curThread = new JoinThread(previousThread);
+            curThread.start();
+           
+            previousThread = curThread;
+        }
+        System.out.println("Main here");
+    }
+
+    static class JoinThread extends Thread {
+        private Thread thread;
+
+        public JoinThread(Thread thread) {
+            this.thread = thread;
+        }
+
+        @Override
+        public void run() {
+            try {
+                thread.join();
+                System.out.println(thread.getName() + " terminated.");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
