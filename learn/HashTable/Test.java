74cf7e733e69bc9c9e60175851bef962bf107d51
Megathrone <yizhouwang418@gmail.com>
2018-05-24 06:41:08 +0800

    HashMap usage

--- learn/HashTable/Test.java
+++ learn/HashTable/Test.java
@@ -1,11 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Test
  */
 public class Test {
 
     public static void main(String[] args) {
-        String str = "abcdefg";
-        System.out.println(str.hashCode());
-        System.out.println();
+        int[] a = {1,2,3,4,5,6,7,7};
+        List<Integer> ans = new ArrayList<>();
+    
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : a) {
+            set.add(num);
+        }
+        System.out.println(set.iterator().hasNext());
     }
 }
