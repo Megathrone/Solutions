aec132f75b6c8b7416bba3184b4f0e257f423cde
Megathrone <yizhouwang418@gmail.com>
2018-05-27 12:17:06 +0800

    2D array

--- src/SpiralMatrix.java
+++ src/SpiralMatrix.java
@@ -2,34 +2,56 @@ import java.util.*;
 
 /**
  * SpiralMatrix
+ * 这个题是螺旋遍历二维数组，核心的思想是 “削” 的思想
+ * 削去最外面一圈，主要问题在于如何判断边界值
  */
 public class SpiralMatrix {
 
     public List<Integer> spiralOrder(int[][] matrix) {
+        if (matrix.length == 0 || matrix == null)
+            return new ArrayList<Integer>();
+
         List<Integer> res = new ArrayList<>();
-        if(matrix.length==0) return res;
-        int m = matrix.length;
-        int n = matrix[0].length;
-        for (int i = 0; i < m; i++) {
-            if (i % 2 == 0) {
-                for (int num : matrix[i]) {
-                    res.add(num);
-                }
-            } else {
-                for (int k = matrix[i].length - 1; k >= 0; k--) {
-                    res.add(matrix[i][k]);
+        int rowBegin = 0;
+        int rowEnd = matrix.length - 1;
+        int colBegin = 0;
+        int colEnd = matrix[0].length - 1;
+
+        while (rowBegin <= rowEnd && colBegin <= colEnd) {
+            // Traverse Right
+            for (int j = colBegin; j <= colEnd; j++) {
+                res.add(matrix[rowBegin][j]);
+            }
+            rowBegin++;
+
+            // Traverse Down
+            for (int j = rowBegin; j <= rowEnd; j++) {
+                res.add(matrix[j][colEnd]);
+            }
+            colEnd--;
+
+            if (rowBegin <= rowEnd) {
+                // Traverse Left
+                for (int j = colEnd; j >= colBegin; j--) {
+                    res.add(matrix[rowEnd][j]);
                 }
             }
+            rowEnd--;
+
+            if (colBegin <= colEnd) {
+                // Traver Up
+                for (int j = rowEnd; j >= rowBegin; j--) {
+                    res.add(matrix[j][colBegin]);
+                }
+            }
+            colBegin++;
         }
 
         return res;
     }
 
     public static void main(String[] args) {
-        int[][] matrix = { { 1, 2, 3, 4 }, 
-                           { 5, 6, 7, 8 }, 
-                           { 9, 10, 11, 12 } 
-                    };
+        int[][] matrix = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
         SpiralMatrix foo = new SpiralMatrix();
         System.out.println(foo.spiralOrder(matrix));
     }
