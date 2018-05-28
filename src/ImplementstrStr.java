aa86343ab9bc4fb9d9d8008b2438af5cca6e36ec
Megathrone <yizhouwang418@gmail.com>
2018-05-28 17:21:47 +0800

    1

---
+++ src/ImplementstrStr.java
@@ -0,0 +1,23 @@
+/**
+ * ImplementstrStr
+ */
+public class ImplementstrStr {
+
+    public int strStr(String haystack, String needle) {
+        int lenh = haystack.length();
+        int lenn = needle.length();
+        if(lenh<lenn){
+            return -1;
+        }else if(lenn==0){
+            return 0;
+        }
+
+        int window = lenh - lenn;
+        for (int i = 0; i <= window; ++i) {
+            if(haystack.substring(i,i+lenn).equals(needle)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
