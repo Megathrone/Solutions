a331bd4370b3e6c2c157cac4d91f83c5c6f6e0c2
Megathrone <yizhouwang418@gmail.com>
2018-05-31 20:55:34 +0800

    1

---
+++ src/RepeatedSubstringPattern.java
@@ -0,0 +1,26 @@
+/**
+ * RepeatedSubstringPattern
+ */
+public class RepeatedSubstringPattern {
+    public boolean repeatedSubstringPattern(String s) {
+        int len = s.length();
+        for (int i = len / 2; i > 0; i--) {
+            if (len % i == 0) {
+                int times = len / i;
+                StringBuilder sb = new StringBuilder();
+                String str = s.substring(0, i);
+                for (int k = 0; k < times; k++) {
+                    sb.append(str);
+                }
+                if (sb.toString().equals(s))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        RepeatedSubstringPattern foo = new RepeatedSubstringPattern();
+        System.out.println(foo.repeatedSubstringPattern("abcabcabcd"));
+    }
+}
