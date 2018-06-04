059369d511c56a47bfba3a7fd1ac9c131d23dccf
Megathrone <yizhouwang418@gmail.com>
2018-06-04 10:36:05 +0800

    Construct from tree

---
+++ src/ConstructStringfromBinaryTree.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ * 这个题首使用递归的思想来做，首先访问根节点然后左右，这是preorder traversal
+ * 然后呢根绝题目的要求对字符串进行构造注意要把根节点放在最前面
+ */
+
+public class className {
+    public String tree2str(TreeNode t) {
+        if (null == t)
+            return "";
+
+        String res = t.val + "";
+        String left = tree2str(t.left);
+        String right = tree2str(t.right);
+
+        if (left == "" && right == "")
+            return res;
+        if (left == "")
+            return res + "()" + "(" + right + ")";
+        if (right == "")
+            return res + "(" + left + ")";
+        return res + "(" + left + ")" + "(" + right + ")";
+
+    }
+}
