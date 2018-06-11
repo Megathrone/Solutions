247f42e491042ec1891193dd56099d24eec7a7de
Megathrone <yizhouwang418@gmail.com>
2018-06-11 17:17:11 +0800

    实现了一个最基础的带寻路功能的DFS

---
+++ learn/Search/DepthFirstSearch.java
@@ -0,0 +1,40 @@
+/**
+ * DFS
+ */
+public class DepthFirstSearch {
+
+    private boolean[] marked;
+    private int[] edgeTo;
+    private final int s;
+
+    public DepthFirstSearch(Graph g, int s) {
+        marked = new boolean[g.V()];
+        edgeTo = new int[g.V()];
+        this.s = s;
+        dfs(g, s);
+    }
+
+    private void dfs(Graph g, int v) {
+        marked[v] = true;
+        for (int w : g.adj(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = w;
+                dfs(g, w);
+            }
+        }
+    }
+
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v))
+            return null;
+        Stack<Integer> path = new Stack<Integer>();
+        for (int x = v; x != s; x = edgeTo[x])
+            path.push(x);
+        path.push(s);
+        return path;
+    }
+}
--- src/Pow.java
+++ src/Pow.java
@@ -5,6 +5,8 @@ public class Pow {
 
     public double myPow(double x, int n) {
         if(x == 0) return 0;
+        if(n == 1) return x;
+        
         return 0.1;
     }
 }
