bd16156cbd0b3aff0834d798caa0b9e008857e7e
Megathrone <yizhouwang418@gmail.com>
2018-06-22 15:41:22 +0800

    2

--- learn/String/TestString.java
+++ learn/String/TestString.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.*;
 
 /**
  * TestString
@@ -7,19 +9,19 @@ public class TestString {
 
     public static void main(String[] args) {
         int[] count = new int[256];
-        String al = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        
+
         String str = "ABRACADABRA";
         int len = str.length();
         for (int i = 0; i < len; i++) {
-            
-            count[str.toCharArray()[i]]++;
+
+        count[str.toCharArray()[i]]++;
         }
 
         for (int i = 0; i < count.length; i++) {
-            if(count[i]!=0){
-                System.out.println((char)i +"出现了 "+ count[i] + "次");
-            }
+        if(count[i]!=0){
+        System.out.println((char)i +"出现了 "+ count[i] + "次");
         }
+        }
+       
     }
 }
