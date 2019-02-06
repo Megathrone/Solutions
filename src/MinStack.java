ac6eba4e6151d739d6aede91a11c34f15fb7aa55
Megathrone <yizhouwang418@gmail.com>
2019-02-05 23:43:38 -0500

    Stack 1

---
+++ src/MinStack.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+class MinStack {
+
+    Stack<Integer> stack, minStack;
+
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (!minStack.isEmpty()) {
+            int min = minStack.peek();
+            if (x <= min) {
+                minStack.push(x);
+            }
+        } else {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        int val = stack.pop();
+        if (val == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
